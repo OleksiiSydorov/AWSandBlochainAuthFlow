0e2764470bf8bb4fc1e7200c6760d3f44854d780
OlkesiiSydorov <oleksii.sydorov@devcom.com>
2022-10-20 15:40:17 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Integrating Amazon Cognito with Ethereum Blockchain
+
+This is demo project for article [Integrating Amazon Cognito with Ethereum Blockchain](https://yia333.medium.com/integrating-amazon-cognito-with-ethereum-blockchain-7e87f1425422)
+
+![Demo](https://github.com/yai333/AWSandBlochainAuthFlow/blob/master/demo.gif?raw=true)
+
+![diagram](https://github.com/yai333/AWSandBlochainAuthFlow/blob/master/sequencediagram.jpg?raw=true)
+
+## Getting started
+There are 2 directories in this project: frontend and server.
+
+### Server backend
+Backend is a serverless application. To deploy serverless API Gateway endpoints, run
+```
+sls deploy --profile xxx
+```
+
+### Frontend
+Frontend is react app, to start react app, run
+```
+npm install
+npm start
+```
+
+### How to test
+Once AWS Api gateway endpoints are deployed, copy the api urls to .env in `/frontend`.
---
+++ server/src/get_nonce/index.js
@@ -0,0 +1,54 @@
+'use strict';
+const AWS = require('aws-sdk');
+const { getNonce, updateNonce } = require('../utils');
+
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true,
+};
+
+module.exports.handler = async (event, context, callback) => {
+    const {
+        queryStringParameters: { address },
+      } = event;
+      if(!address){
+        return {
+            headers,
+            statusCode: 400,
+            body: JSON.stringify({
+                nonce: null
+            })
+        };
+      }
+      try {
+        const {Items: nonces} = await getNonce(address);
+        //user exist return nonce, for sign in by web3
+        if(nonces && nonces.length > 0){
+          const existNonceResponse = AWS.DynamoDB.Converter.unmarshall(nonces[0]);
+          const objResponse = {
+            ...existNonceResponse,
+            ... {eventName: "ExistNonce"}
+          };
+            return {
+                headers,
+                statusCode: 200,
+                body: JSON.stringify(objResponse)
+            };
+        }else{
+            //User not exist, signup and generate new nonce
+            const newNonceResponse = await updateNonce(address);
+            const objResponse = {
+              ...newNonceResponse,
+              ...{eventName: 'Signup.NewNonce'}
+              
+            };
+            return {
+                headers,
+                statusCode: 200,
+                body: JSON.stringify(objResponse)
+            };
+        }
+      } catch (error) {
+        return callback(null, error)
+      }
+}
